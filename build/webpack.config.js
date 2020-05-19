cec4071bc7b5aa0874f0f77e0467ca0943116684
verymuch <gaofeng781x@163.com>
2020-05-19 21:20:31 +0800

    chore: build

---
+++ build/webpack.config.js
@@ -0,0 +1,29 @@
+const TerserPlugin = require('terser-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, '../lib/'),
+    filename: 'very-axios.min.js',
+    sourceMapFilename: 'very-axios.min.map',
+    library: 'very-axios',
+    libraryTarget: 'umd',
+  },
+  node: {
+    process: false,
+  },
+  devtool: 'source-map',
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true, // Must be set to true if using source-maps in production
+        terserOptions: {
+          // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
+        },
+      }),
+    ],
+  },
+};
